aff6cc41d0ec0a4e410650f55e2f714ef279e433
kara320090 <bongbong9292@naver.com>
2025-11-02 17:14:08 +0900

    Book과 Borrower 작성

--- Borrower.java
+++ Borrower.java
@@ -5,29 +5,21 @@
  * @author (작성자 이름)
  * @version (버전 번호 또는 작성한 날짜)
  */
-public class Borrower
-{
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
+public class Borrower {
+    private String name;
+    private int number;
 
-    /**
-     * Borrower 클래스의 객체 생성자
-     */
-    public Borrower()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
+    public Borrower(String name, int number) {
+        this.name = name;
+        this.number = number;
     }
 
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "[Borrower] " + number + " / " + name;
     }
 }
